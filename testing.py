003ab60e87ae5831b31dbeca071b33cdf948b1bd
alexikerd <41974772+alexikerd@users.noreply.github.com>
2019-12-12 22:44:48 -0600

    Add files via upload
    testing multithreading before I implement it into the main code
    
    got it from http://code.activestate.com/recipes/84317/

---
+++ testing.py
@@ -0,0 +1,87 @@
+import numpy as np
+import time
+import copy
+import threading
+from C4Backend import Game, Player
+
+# http://code.activestate.com/recipes/84317/
+# https://www.youtube.com/watch?v=09_LlHjoEiY
+
+
+game_running = False
+thinking = False
+
+
+class Future:
+	def __init__(self,func,*param):
+		self.__done = False
+		self.__result = None
+		self.__status = 'working'
+		self.__C = threading.Condition()
+		self.__T = threading.Thread(target=self.Wrapper,args=(func,param))
+		self.__T.setName("FutureThread")
+		self.__T.start()
+
+	def repr(self):
+		return '<Future at '+hex(id(self))+':'+self.__status+'>'
+
+	def __call__(self):
+		self.__C.acquire()
+		while self.__done==False:
+			self.__C.wait()
+		self.__C.release()
+		a=copy.deepcopy(self.__result)
+		return a
+
+	def Wrapper(self,func,param):
+		self.__C.acquire()
+		try:
+			self.__result=func(*param)
+		except:
+			self.__result="Exception raised within Future"
+		self.__done = True
+		self.__status='self.__result'
+		self.__C.notify()
+		self.__C.release()
+
+
+
+game = Game()
+
+game.playerlist[0].depth = 5
+game.playerlist[1].depth = 5
+
+A = Future(game.playerlist[game.turn].determine_preference,(game.board,0,game.turn+1,game))
+
+print(A())
+
+
+print('done')
+
+
+
+
+
+
+
+
+
+
+
+
+# def think():
+# 	time.sleep(5)
+# 	return 'done'
+
+
+# while game_running==False:
+
+# 	if thinking==False:
+# 		thinking = True
+# 		t1 = threading.Thread(target=think)
+# 		t1.start()
+# 		value = t1.join()
+# 		game_running = True
+# 		thinking = False
+
+# print(value)
